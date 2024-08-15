95d5a70ab863372882861db1c1da461131453fc1
Frank <frank_rdam@live.nl>
2024-08-15 21:25:15 +0200

    add page transitions

--- app.vue
+++ app.vue
@@ -1,9 +1,29 @@
 <template>
-  <NuxtLayout>
-    <Navbar />
-    <div class="page-inner">
+  <div>
+    <NuxtLayout>
       <NuxtPage />
-    </div>
-    <Footer />
-  </NuxtLayout>
+    </NuxtLayout>
+  </div>
 </template>
+
+<style>
+.page-enter-from {
+  transform: translateX(-100%);
+}
+
+.page-leave-to {
+  transform: translateX(100%);
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition-property: all;
+  transition-timing-function: ease;
+  transition-duration: 500ms;
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["assets/style.scss"],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
 });
